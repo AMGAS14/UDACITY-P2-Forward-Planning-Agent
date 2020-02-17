6b67d0488edfcc4f1c3c4c4bb27b4fadded406cd
Antonio Antunes <amgas14@gmail.com>
2020-02-17 00:00:35 +0000

    Create TreePy

---
+++ treePy.py
@@ -0,0 +1,21 @@
+
+# tree utils module with utils methods for acessing tree data Structure
+
+
+class tree :
+
+    def __init__(self):
+
+        self = 1
+
+
+
+
+
+class node :
+
+    State = 1
+    Parent = 1
+    Action = 1
+    PathCost = 1
+
